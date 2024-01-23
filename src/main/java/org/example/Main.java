658c8cdcf19dba19d071f5666ec44f97124ed3fd
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-01-23 22:22:47 +0900

    feat: Spring Context (#1)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+
+        // Spring Context 생성
+        var context = new AnnotationConfigApplicationContext();
 
         Parrot p = new Parrot();
 
