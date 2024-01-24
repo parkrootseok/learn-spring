0e3f59778b30137c2411bc3cf4a45e65731480ee
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-01-25 01:31:05 +0900

    feat: 스테레오타입 사용하여 Bean 등록 (#1)
    - 인스턴스로 등록할 클래스에 @Component
    - @Component를 탐색할 패키지를 @ComponentScan를 사용해 탐색
      - @ComponentScan(basePackages = "XX") : 탐색할 패키지 지정
    - @PostConstructor
      - 객체 생성 후 특정 메서드를 실행하여 초기화를 진행할 수 있도록 할 수 있음

--- src/main/java/org/example/Parrot.java
+++ src/main/java/org/example/Parrot.java
@@ -1,10 +1,25 @@
 package org.example;
 
+import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+/**
+ * 스테레오타입 주석을 이용해 Bean 등록
+ * @Component : Spring Context에 등록하고 싶은 클래스에 명시
+ */
+@Component
 public class Parrot {
 
     private String name;
+
+    /**
+     * @PostConstruct : 객체 생성 후 해당 메소드를 호출하도록 지시하기 위함
+     */
+    @PostConstruct
+    void init() {
+        this.name = "ROOT";
+    }
 
     public String getName() {
         return name;
