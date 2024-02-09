d57c30dd30701fcfb4135cd7724f4a5e2b0b24dc
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-09 16:42:37 +0900

    feat: 책임이 명시된 @Component 어노테이션 사용 (#8)
    - 기존에 사용한 어노테이션과 동일한 기능을 수행하면서 어떠한 역할을 맡고있는지에 대한 책임의 의미도 함께 담고 있는 어노테이션으로 변경

--- src/main/java/org/example/chap4/services/CommentService.java
+++ src/main/java/org/example/chap4/services/CommentService.java
@@ -5,8 +5,9 @@ import org.example.chap4.repositories.Comment;
 import org.example.chap4.repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CommentService {
 
     private final CommentRepository commentRepository ;
