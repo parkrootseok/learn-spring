cb3d01619cdd41327eda492cf03f10a89a28f803
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-02-13 21:40:13 +0900

    feat: 프로토타입 범위를 가지는 Bean 종속성 주입 (#12)
    - Singleton Bean은 Prototype 범위를 가지는 Bean을 주입하여도 Prototype Bean은 초기에 한 번만 생성되어 동일한 Bean을 계속 사용
    - 여러 인스턴스를 생성하여 Bean을 사용하고 싶을 때 prototype 자위를 가지도록 하

--- src/main/java/org/example/chap4/services/CommentService.java
+++ src/main/java/org/example/chap4/services/CommentService.java
@@ -1,10 +1,12 @@
 package org.example.chap4.services;
 
+import org.example.chap4.processor.CommentProcessor;
 import org.example.chap4.proxies.CommentNotificationProxy;
 import org.example.chap4.repositories.Comment;
 import org.example.chap4.repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -13,12 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class CommentService {
 
-    @Autowired
-    private CommentRepository commentRepository ;
-
-    @Autowired
-    @Qualifier("PUSH")
-    private CommentNotificationProxy commentNotificationProxy;
+//    @Autowired
+//    private CommentRepository commentRepository ;
+//
+//    @Autowired
+//    @Qualifier("PUSH")
+//    private CommentNotificationProxy commentNotificationProxy;
 
 //    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
 //        System.out.println("CommentService 생성!");
@@ -26,13 +28,27 @@ public class CommentService {
 //        this.commentNotificationProxy = commentNotificationProxy;
 //    }
 
-    public CommentRepository getCommentRepository() {
-        return commentRepository;
-    }
+//    public CommentRepository getCommentRepository() {
+//        return commentRepository;
+//    }
+//
+//    public void publishComment(Comment comment) {
+//        commentRepository.storeComment(comment);
+//        commentNotificationProxy.sendComment(comment);
+//    }
 
-    public void publishComment(Comment comment) {
-        commentRepository.storeComment(comment);
-        commentNotificationProxy.sendComment(comment);
+
+    @Autowired
+    public CommentProcessor p;
+
+    public void sendComment(Comment comment) {
+
+        p.setComment(comment);
+        p.processComment(comment);
+        p.validateComment();
+
+        comment = p.getComment();
+
     }
 
 
