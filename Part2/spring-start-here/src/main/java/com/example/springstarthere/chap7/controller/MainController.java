1420e5f2a582ff3f593b216d97034017b7270639
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-04 21:17:02 +0900

    chore: move directory (#24)

--- Part2/spring-start-here/src/main/java/com/example/springstarthere/chap7/controller/MainController.java
+++ Part2/spring-start-here/src/main/java/com/example/springstarthere/chap7/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.springstarthere.controller;
+package com.example.springstarthere.chap7.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
