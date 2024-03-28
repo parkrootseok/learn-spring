cdad6428d573b99518188d7687ebda66371c614f
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-03-28 19:06:45 +0900

    feat: @RestController (#31)
    - @RestController
     - @ReponseBody를 HTTP에 데이터를 담아 전송하는 모든 메서드에 추가해야 하는 반복작업을 대신 수행
     - 기존엔 @Controller를 사용했지만, 위 어노테이션으로 변경하면, 모든 메서드에 @ResponseBody 어노테이션 추가 및 @Controller 어노테이션 기능도 사용 가능

--- Part2/spring-start-here/src/main/java/com/example/springstarthere/controller/HelloController.java
+++ Part2/spring-start-here/src/main/java/com/example/springstarthere/controller/HelloController.java
@@ -1,17 +1,17 @@
 package com.example.springstarthere.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+/**
+ * @RestController : 모든 메서드마다 @ResponseBody 주석을 반복하는 것을 피하기 위한 어노테이션
+ * -> 자동으로 모든 메서드에 @ResponseBody 어노테이션을 추가
+ * -> ResponseBody + Controller = RestController
+ */
+@RestController
 public class HelloController {
 
 	@GetMapping("/hello")
-	/**
-	 * @ResponseBody : DispatcherServlet에게 데이터를 View가 아닌 HTTP Response로 직접 전송함을 알리기 위한 어노테이션
-	 */
-	@ResponseBody
 	public String hello() {
 		return "hello";
 	}
