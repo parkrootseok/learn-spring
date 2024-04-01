a09ecd1721beeea711ae569aca1955a2a236ea70
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-04-01 22:28:26 +0900

    feat: ResponseEntity (#31)
    - header를 설정하여 응답 객체를 생성할 수 있음

--- Part2/spring-start-here/src/main/java/com/example/springstarthere/controller/CountryController.java
+++ Part2/spring-start-here/src/main/java/com/example/springstarthere/controller/CountryController.java
@@ -2,6 +2,8 @@ package com.example.springstarthere.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +25,15 @@ public class CountryController {
 
 
 	@GetMapping("/france")
-	public Country france() {
+	public ResponseEntity<Country> france() {
 
 		Country c = Country.of("France", 67);
-		return c;
+		return ResponseEntity
+			.status(HttpStatus.ACCEPTED)
+			.header("continent", "Europe")
+			.header("capital", "Paris")
+			.header("favorite_food", "cheese and whine")
+			.body(c);
 
 	}
 
