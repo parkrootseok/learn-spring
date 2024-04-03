f691ca6b64fde330d4cd8805981a577153eb28f9
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2024-04-03 23:23:29 +0900

    feat: 비즈니스 로직 수행 서버 (#34)

---
+++ Part2/payment-server/src/main/java/com/example/springstarthere/service/PaymentService.java
@@ -0,0 +1,14 @@
+package com.example.springstarthere.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.springstarthere.model.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment processPayment()  {
+		return null;
+	}
+
+}
