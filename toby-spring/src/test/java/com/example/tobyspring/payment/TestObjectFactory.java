bb64ca5bb4ee1c4f5fb3cb02a41b69c5406abab6
박근석 <parkrootseok@gmail.com>
2024-07-20 15:40:32 +0900

    chore: 불필요한 import 제거

--- toby-spring/src/test/java/com/example/tobyspring/payment/TestObjectFactory.java
+++ toby-spring/src/test/java/com/example/tobyspring/payment/TestObjectFactory.java
@@ -1,8 +1,5 @@
 package com.example.tobyspring.payment;
 
-import com.example.tobyspring.exchangerate.CachedExchangeRateProvider;
-import com.example.tobyspring.exchangerate.SimpleExchangeRateProvider;
-import com.example.tobyspring.exchangerate.WebApiExchangeRateProvider;
 import java.math.BigDecimal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
