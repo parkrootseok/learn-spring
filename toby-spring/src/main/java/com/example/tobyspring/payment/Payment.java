7663eb29befc952c0c065ded069cb47bb33cadc1
박근석 <parkrootseok@gmail.com>
2024-07-20 15:39:33 +0900

    chore: 불필요한 코드 삭제 및 추가

--- toby-spring/src/main/java/com/example/tobyspring/payment/Payment.java
+++ toby-spring/src/main/java/com/example/tobyspring/payment/Payment.java
@@ -12,24 +12,48 @@ public class Payment {
     // double, float 형을 사용할 경우 데이터 손실 위험이 있으므로 BigDecimal 사용
     private BigDecimal foreignCurrencyAmount;
 
-    private BigDecimal exRate;
+    private BigDecimal exhangeRate;
 
     private BigDecimal convertedAmount;
 
     private LocalDateTime validUtil;
 
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExhangeRate() {
+        return exhangeRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUtil() {
+        return validUtil;
+    }
+
     public Payment(
             Long orderId,
             String currency,
             BigDecimal foreignCurrencyAmount,
-            BigDecimal exRate,
+            BigDecimal exhangeRate,
             BigDecimal convertedAmount,
             LocalDateTime validUtil
     ) {
         this.orderId = orderId;
         this.currency = currency;
         this.foreignCurrencyAmount = foreignCurrencyAmount;
-        this.exRate = exRate;
+        this.exhangeRate = exhangeRate;
         this.convertedAmount = convertedAmount;
         this.validUtil = validUtil;
     }
@@ -40,7 +64,7 @@ public class Payment {
                 "orderId=" + orderId +
                 ", currency='" + currency + '\'' +
                 ", foreignCurrencyAmount=" + foreignCurrencyAmount +
-                ", exRate=" + exRate +
+                ", exRate=" + exhangeRate +
                 ", convertedAmount=" + convertedAmount +
                 ", validUtil=" + validUtil +
                 '}';
