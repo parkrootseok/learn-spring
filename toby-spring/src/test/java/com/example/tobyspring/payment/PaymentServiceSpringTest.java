11080471ac4f2aa2e3be550d41ee9641fd44c7ff
박근석 <parkrootseok@gmail.com>
2024-07-21 16:02:02 +0900

    test: 유효 시간 검증

--- toby-spring/src/test/java/com/example/tobyspring/payment/PaymentServiceSpringTest.java
+++ toby-spring/src/test/java/com/example/tobyspring/payment/PaymentServiceSpringTest.java
@@ -1,8 +1,10 @@
 package com.example.tobyspring.payment;
 
-import com.example.tobyspring.TestObjectFactory;
+import com.example.tobyspring.TestPaymentConfig;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,11 +13,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestObjectFactory.class)
+@ContextConfiguration(classes = TestPaymentConfig.class)
 class PaymentServiceSpringTest {
 
     @Autowired
     PaymentService paymentService;
+
+    @Autowired
+    Clock clock;
 
     @Autowired
     ExchangeRateProviderStub exchangeRateProviderStub;
@@ -31,6 +36,18 @@ class PaymentServiceSpringTest {
         payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         Assertions.assertThat(payment.getExhangeRate()).isEqualByComparingTo(BigDecimal.valueOf(500));
         Assertions.assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(5_000));
+
+    }
+
+    @Test
+    void validUntil() throws IOException {
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUtil()).isEqualTo(expectedValidUntil);
 
     }
 
--- toby-spring/src/test/java/com/example/tobyspring/payment/PaymentServiceTest.java
+++ toby-spring/src/test/java/com/example/tobyspring/payment/PaymentServiceTest.java
@@ -2,31 +2,53 @@ package com.example.tobyspring.payment;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PaymentServiceTest {
 
+    Clock clock;
+
+    @BeforeEach
+    void beforeEach() {
+        this.clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+    }
+
     @Test
-    void prepare() throws IOException {
+    void testAmount() throws IOException {
 
-        convertedAmount(BigDecimal.valueOf(5_00), BigDecimal.valueOf(5_000));
-        convertedAmount(BigDecimal.valueOf(1_000), BigDecimal.valueOf(10_000));
-        convertedAmount(BigDecimal.valueOf(3_000), BigDecimal.valueOf(30_000));
-
-//        Assertions.assertThat(payment.getValidUtil()).isAfter(LocalDateTime.now());
-//        Assertions.assertThat(payment.getValidUtil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        testAmount(BigDecimal.valueOf(5_00), BigDecimal.valueOf(5_000));
+        testAmount(BigDecimal.valueOf(1_000), BigDecimal.valueOf(10_000));
+        testAmount(BigDecimal.valueOf(3_000), BigDecimal.valueOf(30_000));
 
     }
 
-    private void convertedAmount(BigDecimal exchangeRate, BigDecimal convertedAmount) throws IOException {
+    private void testAmount(BigDecimal exchangeRate, BigDecimal convertedAmount) throws IOException {
 
-        PaymentService paymentService = new PaymentService(new ExchangeRateProviderStub(exchangeRate));
+        PaymentService paymentService = new PaymentService(new ExchangeRateProviderStub(exchangeRate), this.clock);
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
         Assertions.assertThat(payment.getExhangeRate()).isEqualByComparingTo(exchangeRate);
         Assertions.assertThat(payment.getConvertedAmount()).isEqualByComparingTo((convertedAmount));
+
+    }
+
+    @Test
+    void validUntil() throws IOException {
+
+        PaymentService paymentService = new PaymentService(new ExchangeRateProviderStub(BigDecimal.valueOf(1_000)), this.clock);
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUtil()).isEqualTo(expectedValidUntil);
 
     }
 
